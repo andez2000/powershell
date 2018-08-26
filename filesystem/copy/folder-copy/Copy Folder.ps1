17a58a70d07065596617ed849e40e17d40d874f9
andez2000 <andez2000@hotmail.com>
2018-08-26 13:08:24 +0100

    Added copy folder script

---
+++ filesystem/copy/folder-copy/Copy Folder.ps1
@@ -0,0 +1,33 @@
+#//////////////////////////////////////////////////////////////////////////////
+#
+# Filename: Copy Folder.ps1
+#
+# Purpose:  Copy a specified folder and contents to a new location
+#
+# Parameters:
+#   $sourceRoot - the source folder
+#   $destinationRoot - the destination folder  
+#
+# Usage:
+#   & '.\Copy Folder.ps1' -sourceRoot C:\src -destinationRoot C:\dest
+#
+#//////////////////////////////////////////////////////////////////////////////
+param
+(
+  [String]$sourceRoot = '',
+  [String]$destinationRoot = ''
+)
+
+if ($sourceRoot -eq '') {
+    Write-Output "Please specify a source folder";
+    Exit;
+}
+
+if ($destinationRoot -eq '') {
+    Write-Output "Please specify a destination folder";
+    Exit;
+}
+
+Write-Output "Copying ""$sourceRoot"" to ""$destinationRoot"""
+
+Copy-Item -Path $sourceRoot -Filter "*.*" -Recurse -Destination $destinationRoot -Container
