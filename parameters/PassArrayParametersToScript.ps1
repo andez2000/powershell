b31689d32f9b68193accd4c8a552222064be1b00
andez2000 <andez2000@hotmail.com>
2022-05-05 12:15:00 +0100

    added passing array parameters to script example
    PassArrayParametersToScript.ps1

---
+++ parameters/PassArrayParametersToScript.ps1
@@ -0,0 +1,29 @@
+﻿param (
+    [array] $p1,
+    [array] $p2
+)
+
+Write-Host "p1: " $p1
+Write-Host "p2: " $p2
+
+# Examples:
+#
+#    Call:
+#         .\PassArrayParametersToScript.ps1 -p2 5,6,7
+#
+#    Output:
+#        p1:
+#        p2: 5 6 7
+#
+#    Call:
+#         .\PassArrayParametersToScript.ps1 -p1 1,2,3
+#
+#    Output:
+#        p1: 1 2 3
+#        p2: 
+#    Call:
+#         .\PassArrayParametersToScript.ps1 -p1 1,2,3 -p2 5,6,7
+#
+#    Output:
+#        p1: 1 2 3
+#        p2: 5 6 7
